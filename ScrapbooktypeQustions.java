ac50916fde6372a0694a08cf232eab5bea1b167a
KhayroneHans <khayronehans88@gmail.com>
2024-11-06 09:15:35 +0800

    Add files via upload

---
+++ ScrapbooktypeQustions.java
@@ -0,0 +1,34 @@
+
+package javaapplication123;
+
+import java.util.Scanner;
+
+
+public class ScrapbooktypeQustions{
+       public static void main(String[] args) {
+        
+           Scanner scanner = new Scanner(System.in);
+           
+       System.out.println(" 1. WHAT IS YOUR FAVORITE COLOR? ");
+       String name1 = scanner.nextLine();
+       System.out.println(" 2. WHAT IS THE BEST VACATION YOU HAVE EVER BEEN ON? ");
+       String name2 = scanner.nextLine();
+       System.out.println(" 3. WHERE'S YOUR FAVORITE PLACE TO BE? ");
+       String name3 = scanner.nextLine();
+       System.out.println(" 4. WHAT MAKES YOU REALLY ANGRY? ");
+       String name4 = scanner.nextLine();
+       System.out.println(" 5. WHAT PLACE (THAT YOU HAVE NEVER BEEN TO) WOULD YOU LIKE TO TRAVEL TO? " );
+       String name5 = scanner.nextLine();
+       System.out.println(" 6. WHAT IS YOUR FAVORITE MOVIE? ");
+       String name6 = scanner.nextLine();
+       System.out.println(" 7. WHAT IS YOUR FAVORITE FOOD?");
+       String name7 = scanner.nextLine();
+       System.out.println(" 8. WHAT IS YOUR FAVORITE DAY?");
+       String name8 = scanner.nextLine();
+       System.out.println(" 9. WHERE WERE YOU BORN? ");
+       String name9 = scanner.nextLine();
+       System.out.println(" 10. WHEN IS YOUR BIRTHDAY? ");
+       String name10 = scanner.nextLine();
+       }
+    
+}
