5d1dde6f825ad829f87fa8a6fd7bab4d04fa52a7
KhayroneHans <khayronehans88@gmail.com>
2024-11-13 09:17:44 +0800

    Add files via upload

---
+++ Lavoratory_act.java
@@ -0,0 +1,20 @@
+
+package lavoratory_act;
+
+public class Lavoratory_act {
+    public static void main(String[] args) {
+ 
+ int iNumber = 3;
+ if (iNumber >= 0){
+ if (iNumber == 0) {
+     System.out.println("First String"); }
+ 
+ else { System.out.println("Second String");
+        System.out.println("Third String"); }
+ 
+ }
+    }
+        
+ }
+    
+
